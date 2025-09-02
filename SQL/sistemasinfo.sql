9595f2d0aa6ca6f8cb48c1f3ebeac4ef81e6503d
Santiago Gabriel <dominics4n@gmail.com>
2025-09-01 21:10:43 -0600

    sql base

---
+++ SQL/sistemasinfo.sql
@@ -0,0 +1,25 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema sistemas_info
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `sistemas_info` DEFAULT CHARACTER SET utf8 ;
+USE `sistemas_info` ;
+
+-- Tabla de Personas
+CREATE TABLE personas (
+    id_persona INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL
+);
+
+-- Tabla de Oficios
+CREATE TABLE oficios (
+    id_oficio INT PRIMARY KEY AUTO_INCREMENT,
+    numero_oficio VARCHAR(50) NOT NULL,
+    area VARCHAR(100) NOT NULL,
+    fecha DATE NOT NULL,
+    id_persona INT NOT NULL,
+    FOREIGN KEY (id_persona) REFERENCES personas(id_persona)
+);
